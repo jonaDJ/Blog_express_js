beb99678195f2c684cefa237ea893223ad055a7a
jonaDJ <jonathandarla255@gmail.com>
2023-10-25 16:47:17 -0700

    added form add logic-post

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ mongoose
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
 // You have install morgan to use this
 app.use(morgan("dev"));
 
@@ -31,13 +33,21 @@ app.get("/about", (req, res) => {
 
 app.get("/blogs", (req, res) => {
   Blog.find()
-    .sort({ createAt: -1 })
+    .sort({ createdAt: -1 })
     .then((result) =>
       res.render("index", { title: "All Blogs", blogs: result })
     )
     .catch((err) => console.log(err));
 });
 
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => res.redirect("/blogs"))
+    .catch((err) => console.log(err));
+});
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create Blog" });
 });
